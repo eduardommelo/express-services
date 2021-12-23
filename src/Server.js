f3cf822076523169ba2e5343211af7a620ceb21e
eduardo melo <fobbie.gov@gmail.com>
2021-12-22 22:26:04 -0300

    fix: render instance 2

--- src/Server.js
+++ src/Server.js
@@ -147,7 +147,6 @@ module.exports = class Client {
           route,
           this._onMiddlewareStart(fileReq.middleware),
           async (req, res) => {
-            fileReq = new fileReq(this);
             const routeReturn = await fileReq.run(req, res);
             res.render(fileReq.publicURL + ".ejs", routeReturn);
           }
@@ -157,7 +156,6 @@ module.exports = class Client {
           route,
           this._onMiddlewareStart(fileReq.middleware),
           async (req, res) => {
-            fileReq = new fileReq(this);
             fileReq.run(req, res);
           }
         );
